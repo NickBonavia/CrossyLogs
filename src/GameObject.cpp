6c50388ebb1d57824147c6ff2f8673ebc8524329
geoRunstar <geordiemj@gmail.com>
2019-11-22 17:51:27 -0600

    added GameObject class

---
+++ src/GameObject.cpp
@@ -0,0 +1,35 @@
+#include "GameObject.h"
+#include "TextureManager.h"
+
+GameObject::GameObject(const char* tecture_sheet, SDL_Renderer* render, int x, int y)
+{
+	renderer = render;
+	object_texture = TextureManager::LoadTexture(texture_sheet,render);
+
+	x_position = x;
+	y_position =y;
+}
+
+void GameObject::Update()
+{
+
+	source_rect.h =64;
+	source_rect.w =64;
+	source_rect.x =0;
+	source_rect.y =0;
+
+	destination_rect.x = x_position;
+	destination_rect.y = y_position;
+
+	destination_rect.w = source_rect.w;
+
+	destination_rect.h = source_rect.h;
+
+}
+
+void GameObject::Render()
+{
+	SDL_RenderCopy(renderer,object_texture, &source_rect, &destination_rect);
+	
+}
+
