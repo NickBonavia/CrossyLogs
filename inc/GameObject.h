7b37f7c686ff774649090dcd06c31b181f401f0b
NickBonavia <nick.bonavia@tcu.edu>
2019-12-04 15:40:57 -0600

    Changed interface

--- inc/GameObject.h
+++ inc/GameObject.h
@@ -4,41 +4,29 @@ class GameObject{
 
 
 	public:
-		virtual	~GameObject(){};
+		int x_position;
+		int y_position;
+		SDL_Rect destination_rect;
 
+		virtual	~GameObject(){};
 		virtual void Update(double delta)=0;
 		void Render()
 		{			
-		SDL_RenderCopy(renderer,object_texture, &source_rect, &destination_rect);
+			SDL_RenderCopy(renderer,object_texture, nullptr, &destination_rect);
 		}
-	protected:
 		
 		GameObject(SDL_Texture* obj_text, SDL_Renderer* render, int x, int y)
 		{
-		 renderer=render; 
-		object_texture=obj_text;
-		 x_position=x; 
-		 y_position=y; }
-
-
-		void setTexture(SDL_Texture* obj_text){object_texture=obj_text;}
-		int getX(){ return x_position;}
-		int getY(){ return y_position;}
-		void setX(int x){ x_position=x;}
-		void setY(int y){ y_position=y;}
-		SDL_Rect getSourceRect(){ return source_rect;}
-		SDL_Rect getDestinationRect(){ return destination_rect;}
-
-		void setSourceRect(SDL_Rect sr){  source_rect=sr;}
-		void setDestinationRect(SDL_Rect dr){ destination_rect=dr;}
-
-		SDL_Rect source_rect, destination_rect;
+			renderer = render; 
+			object_texture = obj_text;
+			x_position = x; 
+			y_position = y;
+			destination_rect.x = x;
+			destination_rect.y = y;			
+			destination_rect.w = 64;
+			destination_rect.h = 64;
+		}
+protected:
 		SDL_Renderer* renderer;
 		SDL_Texture* object_texture;
-
-		int x_position;
-		int y_position;
-
-
-
 };
